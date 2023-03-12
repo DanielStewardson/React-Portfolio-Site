f78a38fd6236bed21de5cae8441a43d16957b437
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2023-03-12 08:04:29 +0000

    set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact';
+import Navbar from './components/Navbar/Navbar';
+import ThemeButton from './components/ThemeButton/ThemeButton';
 
 function App() {
+
+  const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <div id='content-box'>
+        <div>
+          <Navbar />
+          <ThemeButton onClick={toggleTheme} theme={theme}/>
+        </div>
+        <Routes>
+          <Route path='/' element={ <Home/> } />
+          <Route path='projects' element={ <Projects/> } />
+          <Route path='about' element={ <About/> } />
+          <Route path='contact' element={ <Contact/> } />
+        </Routes>
+      </div>
     </div>
   );
 }
