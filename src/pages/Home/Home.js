28bf542f34691e66e4d652f50bfa1b4364f868f6
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2023-03-12 19:30:43 +0000

    moved header to own component so it could be moved around between pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default function Home() {
   return (
     <div id='home'>
-        <header id='welcome-message'>
-            <h1>Daniel Stewardson</h1>
-            <p>Front-end web developer</p>
-            <div id='call-to-action-buttons'>
-                <Link to='/Projects'>&#x2192; See my projects</Link>
-                <Link to='/Contact'>&#x2192; Contact me</Link>
-            </div>
-        </header>
+      Home
     </div>
   )
 }
